68938b679649e4aabba408070ca14492d29029e1
Hector Urtubia <hector@olark.com>
2016-06-13 23:20:54 -0700

    Shell filesw

--- renderer.js
+++ renderer.js
@@ -1,3 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+var fs = require('fs');
+var { shell } = require('electron');
+
+var files = fs.readdirSync('.');
+files.map( (file) => {
+  var newDiv = document.createElement('div');
+  newDiv.innerHTML = "<b onClick=openFile('" + file + "')>" + file + "</b>";
+  document.querySelector('#files').appendChild(newDiv);
+});
+
+window.openFile = function(theFile) {
+  alert(theFile);
+  shell.openItem(theFile);
+}
